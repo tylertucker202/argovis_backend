28bbc331941ced6e95257647e3689662c35c11ac
tylertucker202 <tylertucker202@yahoo.com>
2018-05-23 14:12:11 -0700

    summerPatch start

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
   db: {
     //production: process.env.MONGODB_URI || 'mongodb://genericUser:genericUser@ds161483.mlab.com:61483/argo_test',
-    production:  process.env.MONGODB_URI || 'mongodb://localhost/argo2',
+    production:  process.env.MONGODB_URI || 'mongodb://localhost/argoTrouble',
     development: 'mongodb://localhost/argo2',
     test: 'mongodb://localhost/argo2'
   }
