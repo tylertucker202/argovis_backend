65dea8d98b348a0e3ca0ca5ce6f93802693e2b6c
tylertucker202 <tylertucker202@yahoo.com>
2018-07-15 11:28:31 -1000

    merge fix

--- config/default.js
+++ config/default.js
@@ -2,12 +2,7 @@ module.exports = {
   db: {
     //production: process.env.MONGODB_URI || 'mongodb://genericUser:genericUser@ds161483.mlab.com:61483/argo_test',
     production:  process.env.MONGODB_URI || 'mongodb://localhost/argo',
-<<<<<<< HEAD
     development: 'mongodb://localhost/argoTrouble',
-    test: 'mongodb://localhost/argo2'
-=======
-    development: 'mongodb://localhost/argo',
     test: 'mongodb://localhost/argo'
->>>>>>> 8ee24ff717d9a4ba29ba7582143d88883b63882b
   }
 };
