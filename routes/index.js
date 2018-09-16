d4864256c7cdd342ab282bb655b90fb635c4e0c5
tylertucker202 <tylertucker202@yahoo.com>
2018-09-16 13:59:45 -1000

    init commit for ngArgo

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-var Profile = require('../models/profile'); 
-var async = require('async');
-var moment = require('moment')
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.redirect('/map');
+/* GET ng home page. */
+router.get('/', function(req, res, next) {
+  res.render('dist/index');
 });
+
+// /* GET home page. */
+// router.get('/', function(req, res) {
+//   res.redirect('/map');
+// });
 
 router.get('/map', function(req,res) {
         res.render('map_web_mercator', {
