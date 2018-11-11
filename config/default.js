242f048d57aa2ec449baf4004801d01d04ac028f
tylertucker202 <tylertucker202@gmail.com>
2018-11-11 10:24:48 -1000

    updating for new database on droplet

--- config/default.js
+++ config/default.js
@@ -2,15 +2,15 @@ module.exports = {
   db: {
     //production: process.env.MONGODB_URI || 'mongodb://genericUser:genericUser@ds161483.mlab.com:61483/argo_test',
     production:  process.env.MONGODB_URI || 'mongodb://localhost/argo',
-    development: 'mongodb://localhost/argo',
-    docker: 'mongodb://database/argo',
-    test: 'mongodb://localhost/argo'
+    development: 'mongodb://localhost/argo2',
+    docker: 'mongodb://database/argo2',
+    test: 'mongodb://localhost/argo2'
   },
 
   startDate: {
     production: 'today',
     development: '2017-12-20',
-    docker: '2017-12-20',
-    test: '2017-12-20'
+    docker: '2018-11-10',
+    test: '2018-11-10'
   }
 };
