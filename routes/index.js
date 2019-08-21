d89fa1c139614cead9c17dedad3d27843294fa4f
tylertucker202 <tylertucker202@gmail.com>
2019-08-20 15:01:01 -1000

    removed old routs

--- routes/index.js
+++ routes/index.js
@@ -17,14 +17,6 @@ router.get('/ng/home', function(req, res, next) {
   res.sendFile(mpath);
 });
 
-router.get('/map', function(req,res) {
-        res.render('map_web_mercator', {
-            lat : 0,
-            lng : 0
-        });
-});
-
-
 router.get('/covarMap', function(req,res) {
   res.render('covarPage', {
       lat : 0,
@@ -32,36 +24,8 @@ router.get('/covarMap', function(req,res) {
   });
 });
 
-
-
 router.get('/grid', function(req,res) {
   res.render('gridPage', {
-      lat : 0,
-      lng : 0
-  });
-});
-
-
-router.get('/mapNPS', function(req,res) {
-  res.render('map_north_polar_stereo', {
-      lat : 89,
-      lng : 0.1
-  });
-});
-
-router.get('/geotiff', function(req,res) {
-  res.render('d3grid');
-});
-
-router.get('/mapSPS', function(req,res) {
-  res.render('map_south_polar_stereo', {
-      lat : -89,
-      lng : -0.1
-  });
-});
-
-router.get('/mapEQUIREC', function(req,res) {
-  res.render('map_equireq_proj', {
       lat : 0,
       lng : 0
   });
