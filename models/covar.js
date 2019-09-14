2e47e4bc47b650b2ef5f764f4fc69719b0879442
tylertucker202 <tylertucker202@gmail.com>
2019-09-13 16:15:59 -1000

    updated covar model

--- models/covar.js
+++ models/covar.js
@@ -6,7 +6,9 @@ var CovarSchema = Schema(
     forcastDays: {type: Number, required: true },
     _id: {type: String, required: true, max: 100},
     features: [{type: Schema.Types.Mixed, required: true}],
-    geoLocation: {type: Schema.Types.Mixed, required: true}
+    geoLocation: {type: Schema.Types.Mixed, required: true},
+    dLat: {type: Number, required: true },
+    dLong: {type :Number, required: true}
   },
   );
 
