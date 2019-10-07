00e1ddf9944aec5f63517c3379894953957918e2
tylertucker202 <tylertucker202@gmail.com>
2019-10-06 14:50:21 -1000

    api for grid param

--- controllers/griddedProductController.js
+++ controllers/griddedProductController.js
@@ -3,6 +3,16 @@ var GridParam = require('../models/gridParam');
 var GJV = require('geojson-validation');
 var moment = require('moment');
 
+const get_grid_name= function(grid, param) {
+    let trend = 'Trend'
+    if (grid.contains('NoTrend')) {
+        trend = 'NoTrend'
+    }
+    const paramTrend = param + trend
+    const gridName = grid.replace(trend, paramTrend)
+    return gridName
+}
+
 const get_grid_model = function(grid) {
     let GridModel
     switch(grid) {
@@ -48,29 +58,23 @@ exports.find_one = function(req, res , next) {
 }
 
 exports.find_grid_param = function(req, res , next) {
-    req.sanitize('measurement').escape();
-    req.sanitize('measurement').trim();
+    req.sanitize('grid').escape();
+    req.sanitize('grid').trim();
     req.sanitize('pres').escape();
     req.sanitize('pres').trim();
-    req.sanitize('model').escape();
-    req.sanitize('model').trim();
-    req.sanitize('trend').escape();
-    req.sanitize('trend').trim();
     req.sanitize('param').escape();
     req.sanitize('param').trim();
 
     req.checkParams('pres', 'pres should be numeric.').isNumeric();
-    req.checkParams('model', 'model should be string.').isAlpha();
-    req.checkParams('trend', 'trend should be alphanumeric string (Trend, Trend2, NoTrend).').isAlphanumeric();
     req.checkParams('param', 'param should be string.').isAlpha();
+    req.checkParams('grid', 'grid should be string.').isAlphanumeric();
+    const pres = JSON.parse(req.query.pres)
+    const grid = req.query.grid
+    const param = req.query.param
 
-    const pres = req.params.pres
-    const model = req.params.model
-    const trend = req.params.trend
-    const param = req.params.param
-
-    console.log(req.params)
-    const query = GridParam.find({pres: pres, model: model, trend: trend, param: param}, {model: 1, param:1, measurement: 1, trend: 1, pres: 1});
+    const gridName = get_grid_name(grid, param)
+    
+    const query = GridParam.find({pres: pres, gridName}, {model: 1, param:1, measurement: 1, trend: 1, pres: 1});
     query.limit(1)
     query.exec( function (err, grid) {
         if (err) { return next(err); }
@@ -79,7 +83,7 @@ exports.find_grid_param = function(req, res , next) {
 }
 
 
-exports.get_window = function(req, res , next) {
+exports.get_grid_window = function(req, res , next) {
     req.sanitize('grid').escape();
     req.sanitize('grid').trim();
 
@@ -123,8 +127,6 @@ exports.get_window = function(req, res , next) {
     }
 
     let GridModel = get_grid_model(gridStr)
-
-    console.log(pres, lonRange, latRange, monthYear.toDate())
 
     var query = GridModel.aggregate([
         {$match: {pres: pres}},
@@ -172,6 +174,89 @@ exports.get_window = function(req, res , next) {
             cellYSize: -1,
             noDataValue: -1,
             gridName: -1,
+            nRows: { $size: '$lats'},
+            nCols: { $size: '$lons'},
+            xllCorner: { $min: '$lons'},
+            yllCorner: { $min: '$lats'},
+            zs: 1,
+            },
+        }
+        ]);
+    
+    query.exec( function (err, grid) {
+        if (err) { return next(err); }
+        res.json(grid);
+    });
+}
+
+exports.get_param_window = function(req, res , next) {
+    req.sanitize('grid').escape();
+    req.sanitize('grid').trim();
+    req.sanitize('pres').escape();
+    req.sanitize('pres').trim();
+    req.sanitize('latRange').escape();
+    req.sanitize('latRange').trim();
+    req.sanitize('lonRange').escape();
+    req.sanitize('lonRange').trim();
+    req.sanitize('param').escape();
+    req.sanitize('param').trim();
+
+    req.checkParams('pres', 'pres should be numeric.').isNumeric();
+    req.checkParams('param', 'param should be string.').isAlpha();
+    req.checkParams('grid', 'grid should be string.').isAlphanumeric();
+    const pres = JSON.parse(req.query.pres)
+    const grid = req.query.grid
+    const param = req.query.param
+
+    const latRange = JSON.parse(req.query.latRange)
+    const lonRange = JSON.parse(req.query.lonRange)
+    const gridName = get_grid_name(grid, param)
+
+    var query = GridParam.aggregate([
+        {$match: {pres: pres, gridName}},
+        {$project: { // query for lat lng ranges
+            pres: -1,
+            cellsize: -1,
+            NODATA_value: -1,
+            gridName: -1,
+            measurement: -1,
+            data: {
+                $filter: {
+                    input: '$data',
+                    as: 'item',
+                    cond: {
+                        $and: [
+                            {$gt: ['$$item.LATITUDE', latRange[0]]},
+                            {$lt: ['$$item.LATITUDE', latRange[1]]},
+                            {$gt: ['$$item.LONGITUDE', lonRange[0]]},
+                            {$lt: ['$$item.LONGITUDE', lonRange[1]]}
+                        ]},
+                },
+            },
+        }},
+        { $unwind : '$data' }, //allows sorting
+        {$sort:  {'data.LATITUDE': -1, 'data.LONGITUDE': 1}},
+        {$group: {_id: '$_id', //collection for nrows and ncolumns
+                        'pres': {$first: '$pres'},
+                        'measurement': {$first: '$measurement'},
+                        'cellXSize': {$first: '$cellsize'},
+                        'cellYSize': {$first: '$cellsize'},
+                        'noDataValue': {$first: '$NODATA_value'},
+                        'gridName': {$first: '$gridName'},
+                        'lons': { $addToSet: "$data.LONGITUDE" },
+                        'lats': { $addToSet: "$data.LATITUDE"},
+                        'zs': {$push : "$data.value" // values should be in sorted order
+                                },
+          
+            }
+        },
+        {$project: {
+            pres: -1,
+            cellXSize: -1,
+            cellYSize: -1,
+            noDataValue: -1,
+            gridName: -1,
+            measurement: -1,
             nRows: { $size: '$lats'},
             nCols: { $size: '$lons'},
             xllCorner: { $min: '$lons'},
--- routes/griddedProducts.js
+++ routes/griddedProducts.js
@@ -4,10 +4,11 @@ var router = express.Router();
 // Require controller modules
 var grid_controller = require('../controllers/griddedProductController')
 
-router.get('/:grid/window', grid_controller.get_window);
+router.get('/:grid/window', grid_controller.get_grid_window);
 
 router.get('/:grid/find', grid_controller.find_one);
 
-router.get('/gridParams/:measurement/:pres/:model/:trend/:param', grid_controller.find_grid_param);
+router.get('/gridParams/find', grid_controller.find_grid_param);
+router.get('/gridParams/window', grid_controller.get_param_window)
 
 module.exports = router;
