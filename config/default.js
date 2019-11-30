f89d53995e023a8dfbba0aa9f53777de9abc3291
tylertucker202 <tylertucker202@gmail.com>
2019-11-29 16:54:13 -1000

    jg data option

--- config/default.js
+++ config/default.js
@@ -3,13 +3,15 @@ module.exports = {
     production:  process.env.MONGODB_URI || 'mongodb://localhost/argo',
     development: 'mongodb://localhost/argo',
     docker: 'mongodb://database/argo',
-    test: 'mongodb://localhost/argo-express-test'
+    test: 'mongodb://localhost/argo-express-test',
+    jg: 'mongodb://localhost/JG'
   },
 
   startDate: {
     production: 'yesterday',
     development: '2017-12-20',
     docker: 'yesterday',
-    test: '2018-11-10'
+    test: '2018-11-10',
+    jg: '2017-12-20',
   }
 };
