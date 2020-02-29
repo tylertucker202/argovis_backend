f349179d66f02442d807aabb44e5418ce1a77f8b
tylertucker202 <tylertucker202@gmail.com>
2020-02-29 08:01:32 -1000

    dev merge

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,10 @@
-<<<<<<< HEAD
-var express = require('express');
-var router = express.Router();
-<<<<<<< HEAD
-var path = require('path');
-=======
 const express = require('express');
 let router = express.Router();
 const path = require('path');
->>>>>>> dev
 
 /* GET ng home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/ng/home')
-=======
-var mongoose = require('mongoose');
-var Profile = require('../models/profile'); 
-var async = require('async');
-var moment = require('moment')
-
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.redirect('/map');
->>>>>>> 2e2c33800c8bb896fc1378632764f41bd548f047
 });
 
 router.get('/ng/*', function(req, res, next) {
